0d303cd86f91b28c3c9cd7a66c45a12ca36c362a
Geng-Wung <gengxis92@gmail.com>
2022-08-19 18:48:55 -0400

    cleaned center added dynamic color links

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const h2 = document.createElement("h2");
 
-h2.textContent = "Where you will find all the things Geng finds cool";
+h2.textContent = "Where you will find things that Geng thinks are cool";
 
 document.querySelector("header").appendChild(h2);
