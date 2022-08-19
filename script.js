a169f1d48f697bf353251cae62c9402a17a7904f
Geng-Wung <gengxis92@gmail.com>
2022-08-19 11:16:15 -0400

    adjustment to html and CSS, created new sections

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-
 const h2 = document.createElement("h2");
 
 h2.textContent = "Where you find all the the thing Geng finds cool about the internet";
 
-document.querySelector("body").appendChild(h2);
+document.querySelector("header").appendChild(h2);
