6b1aa15acab84a8fd9dd512e52f9de4b3b02be83
Geng-Wung <gengxis92@gmail.com>
2022-08-19 15:35:04 -0400

    retrying to move images to right side

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const h2 = document.createElement("h2");
 
-h2.textContent = "Where you will find all the things Geng finds cool about the internet!";
+h2.textContent = "Where you will find all the things Geng finds cool";
 
 document.querySelector("header").appendChild(h2);
