c236d54dd8050bf9391c0d639692a4006656457a
Geng-Wung <gengxis92@gmail.com>
2022-08-19 12:15:33 -0400

    added off white border and center text in header

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const h2 = document.createElement("h2");
 
-h2.textContent = "Where you find all the the thing Geng finds cool about the internet";
+h2.textContent = "Where you will find all the the thing Geng finds cool about the internet";
 
 document.querySelector("header").appendChild(h2);
